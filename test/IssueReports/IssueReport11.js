c06fe77a759551a4f63e07e96eeb4bd5c5fa06a4
Cole Millsap <cole@poweriep.com>
2015-06-11 11:08:10 -0500

    Added test cases to insure that parent properties are not changed. Added test cases to verify that calling function via 'super' was functioning correctly.

--- test/IssueReports/IssueReport11.js
+++ test/IssueReports/IssueReport11.js
@@ -44,15 +44,24 @@ test('IssueReports:IssueReport11', function(assert) {
     var B = Class({'extends': A}, {
         'public function f3': function() {
             this.f2();
+        },
+        'public function f4': function() {
+            this.super('f2');
         }
     });
     var b = new B();
     assert.equal(typeof(b.f3), 'function', 'f3 exists on the child');
     assert.equal(typeof(b.f2), 'function', 'f2 exists on the child');
     assert.equal(typeof(b.f1), 'function', 'f1 exists on the child');
+    b.f3();
     b.super('f2');
     b.super('f1');
     assert.equal(b.getA(), 'A', 'Parent method f1 called from parent within scope of child');
     assert.equal(b.getB(), 'B', 'Parent method f2 called within scope of child');
-    assert.equal(b.getA(), 'A', 'Parent method f1 called from parent within scope of child');
+    var b2 = new B();
+    assert.equal(b2.getA(), null, 'New instance of model property A set correctly');
+    assert.equal(b2.getB(), null, 'New instance of model property B set correctly');
+    b2.f4();
+    assert.equal(b2.getA(), 'A', 'Parent method f1 called from parent within scope of child with use of "super"');
+    assert.equal(b2.getB(), 'B', 'Parent method f2 called within scope of child with use of "super"');
 });
